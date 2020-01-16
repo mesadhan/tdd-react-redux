e70d3794215f1a5ff41a388910a7fec8eec24e71
mesadhan <sadhansarker3@gmail.com>
2020-01-16 18:44:48 +0600

    added image and css

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './style.scss';
+import Logo from './../../assets/logo.png'
 
 const Header = (props) => {
     return (
         <header>
+            <div className="wrap">
+                <div className="logo">
+                    <img src={Logo} alt="logo"/>
+                </div>
+            </div>
            Hello from React Component
         </header>
     )
