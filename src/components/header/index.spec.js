1d1dc10f9baad6c54f4204dc6620513e8f2a6c5f
mesadhan <sadhansarker3@gmail.com>
2020-01-19 13:03:26 +0600

    make little bit clear using beforeEach()

--- src/components/header/index.spec.js
+++ src/components/header/index.spec.js
@@ -8,16 +8,18 @@ const setUp =(props = {}) => {
 }
 
 describe('Index Component', () => {
+
+    let component;
+    beforeEach(()=> {
+        component = setUp();
+    });
+
     it('Should render without error', () => {
-        const component = setUp();
         const wrapper = component.find('.headerComponent');
-        //console.log(component.debug());
-        //console.log('message', wrapper.length);
         expect(wrapper.length).toBe(1)
     })
 
     it('Should render Image Logo', ()=> {
-        const component = setUp();
         const logoImage = component.find('.logoImage');
         expect(logoImage.length).toBe(1);
     });
