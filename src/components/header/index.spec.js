408acca1b53256d3788cf7734c27bafc5b26d6fb
mesadhan <sadhansarker3@gmail.com>
2020-01-19 13:01:19 +0600

    optimized to avoid everytime initalization

--- src/components/header/index.spec.js
+++ src/components/header/index.spec.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './index';
 
-
+const setUp =(props = {}) => {
+    return shallow(<Header {...props}/>);
+}
 
 describe('Index Component', () => {
     it('Should render without error', () => {
-        const component = shallow(<Header/>);
+        const component = setUp();
         const wrapper = component.find('.headerComponent');
         //console.log(component.debug());
         //console.log('message', wrapper.length);
@@ -15,7 +17,7 @@ describe('Index Component', () => {
     })
 
     it('Should render Image Logo', ()=> {
-        const component = shallow(<Header/>);
+        const component = setUp();
         const logoImage = component.find('.logoImage');
         expect(logoImage.length).toBe(1);
     });
