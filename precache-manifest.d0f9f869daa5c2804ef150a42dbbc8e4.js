ff1ea720f76417edbe165d420698253752b5153e
mesadhan <sadhansarker3@gmail.com>
2020-01-20 19:37:32 +0600

    Updates

---
+++ precache-manifest.d0f9f869daa5c2804ef150a42dbbc8e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303d994b0c4462d068d3cfe86cac483d",
+    "url": "/tdd-react-redux/index.html"
+  },
+  {
+    "revision": "0e3816eb03d582c0bfc0",
+    "url": "/tdd-react-redux/static/css/main.6d799063.chunk.css"
+  },
+  {
+    "revision": "519a587e5d22d1893441",
+    "url": "/tdd-react-redux/static/js/2.7ed0ed3f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tdd-react-redux/static/js/2.7ed0ed3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e3816eb03d582c0bfc0",
+    "url": "/tdd-react-redux/static/js/main.08cae0f4.chunk.js"
+  },
+  {
+    "revision": "c1f34116282138074b16",
+    "url": "/tdd-react-redux/static/js/runtime-main.ee975718.js"
+  }
+]);
